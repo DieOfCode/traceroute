1ff876b76dbf7497974cfad059f68c65f547f67f
DieOfCode <fr0ststone878@gmail.com>
2020-12-17 10:33:52 +0500

    add typing

--- trace/result_message.py
+++ trace/result_message.py
@@ -1,5 +1,5 @@
 class ResultMessage:
-    def __init__(self, results, ttl, icmp_type, icmp_code, tcp_flag) -> object:
+    def __init__(self, results, ttl, icmp_type, icmp_code, tcp_flag):
         self.ttl: int = ttl
         self.icmp_type: int = icmp_type
         self.icmp_code: int = icmp_code
